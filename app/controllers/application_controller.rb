1d1955ab051e18d5542db542f62b5183e798d5e6
Aschonely <aschonely@gmail.com>
2020-03-04 18:37:14 -0500

    Added helper methods to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?, :require_login
+
+  def logged_in?
+    !!current_user
+  end
+    
+  def current_user
+   current_user =  User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to '/' unless session.include? :user_id
+  end
 end
