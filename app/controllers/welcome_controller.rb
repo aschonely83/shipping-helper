ef683ee7dcb720b1fb9ffe360b423621e64c827d
Aschonely <aschonely@gmail.com>
2020-03-07 18:10:30 -0500

    added welcome controller and view, updated routes

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+  def home
+  end
+
+  def signup
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-
+  root "welcome#home"
+  get "/signup", to: "welcome#signup"
+  post "/signup", to: "user#create"
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
+  resources :users, only[:create, :show]
+  resources :retailers
+  
   resources :finished_products, only: [:new, :create, :edit, :update, :destroy]
 
-  root 'users#show'
 end
