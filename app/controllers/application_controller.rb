95db4769a205f5b789ecdceb162742e1f389fbd7
Aschonely <aschonely@gmail.com>
2020-03-12 14:27:52 -0400

    Updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,22 @@
 class ApplicationController < ActionController::Base
-  helper_method :verified_user
   helper_method :current_user
   helper_method :user_is_authenticated
   helper_method :require_logged_in 
   
     private
   
-    def verified_user
-      redirect_to '/' unless user_is_authenticated 
-    end
-  
     def user_is_authenticated
       !!current_user
     end
       
     def current_user
-     current_user =  User.find_by(id: session[:user_id])
+      User.find_by(id: session[:user_id])
     end
   
     def require_logged_in
-      redirect_to controller: 'sessions', actions: 'new', alert: "Please log in" unless user_is_authenticated
+      unless user_is_authenticated
+        flash[:errors] = "Please Log In to continue"
+        redirect_to controller: 'sessions', action: 'new'
+      end
     end 
 end
