d54ec7a795ededbb874b8b050d905e319c056c40
Aschonely <aschonely@gmail.com>
2020-03-12 09:48:48 -0400

    Built methods in finished products controller

--- app/controllers/finished_products_controller.rb
+++ app/controllers/finished_products_controller.rb
@@ -1,18 +1,21 @@
 class FinishedProductsController < ApplicationController
   def new
+    @finished_product = Finished_Product.new
   end
 
   def index
-
+    @finished_products = Finished_Product.all
   end
 
   def create
-
+    @finished_product = Finished_Product.new(finished_product_params)
+    finished_product.save
+    redirect_to retailer_finished_product_path
   end
 
   private
 
-  def finished_products_params
-    
+  def finished_product_params
+    params.require(:finished_product).permit(:product, :quantity, :user_id, :retailer_id)
   end
 end
