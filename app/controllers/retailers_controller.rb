cb762f62ba8f99a0a4e4089c0283f3c0431fadcf
Aschonely <aschonely@gmail.com>
2020-03-12 09:15:28 -0400

    Built methods for retailers controller

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,18 +1,28 @@
 class RetailersController < ApplicationController
   def new
+    @retailer = Retailer.new
   end
 
   def index
-
+    @retailers = Retailer.all
   end
 
   def create
+    @retailer = Retailer.new(retailer_params)
+    if @retailer.save
+      redirect_to retailers_path
+    else  
+      render :new
+    end
+  end
 
+  def show
+    @retailer = Retailer.find(params[:id])
   end
 
   private
 
   def retailer_params
-    
+    params.required(:retailer).permit(:name, :schedule)
   end
 end
