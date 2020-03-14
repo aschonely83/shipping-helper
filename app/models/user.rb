cd52dfa117fda67c21b338c6115fffc5953a2f25
Aschonely <aschonely@gmail.com>
2020-03-14 10:28:51 -0400

    Update retailer and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ActiveRecord::Base
   
   
   has_many :finished_products
-  has_many :retailers, through: :finished_products
+  has_many :retailers, through: => :finished_products
 end
