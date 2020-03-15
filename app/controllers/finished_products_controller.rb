9f880fbaa2cf97f1938482514341fd3bb7a2da85
Aschonely <aschonely@gmail.com>
2020-03-14 21:16:13 -0400

    Cleaned up the code a bit

--- app/controllers/finished_products_controller.rb
+++ app/controllers/finished_products_controller.rb
@@ -1,20 +1,66 @@
 class FinishedProductsController < ApplicationController
-
-  def new
-    @finished_product = Finished_Product.new
-  end
+  before_action :authenticate_user!
+  before_action :get_retailer
+  before_action :get_user
+  before_action :set_finished_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @finished_products = Finished_Product.all
+    if params.include?(:retailer_id)
+      @finished_products = @retailer.finished_products
+        
+    else
+      @finished_products = @user.finshed_products
+         
+    end
+  end
+
+  def show
+  end
+      
+  def new
+    @finished_product = @user.finished_products.build
   end
 
   def create
-    @finished_product = Finished_Product.new(finished_product_params)
-    finished_product.save
-    redirect_to retailer_finished_product_path(@finished_product.retailer, @finished_product)
+    @finished_product = @user.finished_products.build(finished_product_params)
+    @finished_product.retailer_id = @retailer.id
+    if @finished_product.save
+      redirect_to retailer_finished_products_path(@retailer)
+    else
+      render :new
+    end
+  end
+
+  def edit
+       
+  end
+
+  def update
+    if @finished_product.update(finished_product_params)
+      redirect_to retailer_finished_product_path(@finished_product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @finished_product.destroy
+    redirect_to retailer_finished_products_path
   end
 
   private
+
+  def get_user
+    @user = current_user
+  end
+
+  def get_retailer
+    @retailer = Retailer.find_by(params[:retailer_id])
+  end
+
+  def set_finished_product
+    @finished_product= @retailer.finished_products.find(params[:id])
+  end
 
   def finished_product_params
     params.require(:finished_product).permit(:product, :quantity, :user_id, :retailer_id)
--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,33 +1,54 @@
 class RetailersController < ApplicationController
   
-  def new
-    @retailer = Retailer.new
-  end
-    
-  def create
-    @retailer = Retailer.new(retailer_params)
-    if @retailer.save
-      redirect_to retailers_path(@retailer)
-    else  
-      render :new
-    end
-  end
-    
+  before_action :authenticate_user!
+  before_action :set_retailer, only: [:show, :edit, :update, :destroy]
+
   def index
-    @retailers = Retailer.all
+     @retailers = Retailer.all
   end
-    
+
   def show
-    @Retailer = Retailer.find(params[:id])
+     @retailer = Retailer.find(params[:id])
   end
-    
+
+  def new
+      @retailer = Retailer.new
+      @retailer.finished_products.build
+  end
+
   def edit
-        
   end
-    
+
+  def create
+      @retailer = Retailer.new(retailer_params)
+      if @retailer.save
+          redirect_to retailer_path(@retailer)
+      else
+          render :new
+      end
+  end
+
+  def update
+      if @retailer.update(retailer_params)
+          redirect_to retailer_path(@retailer)
+      else 
+          render :edit
+      end
+  end
+
+  def destroy
+      @retailer.destroy
+      redirect_to retailers_path
+  end
+
   private
-    
-  def retailer_params
-    params.required(:retailer).permit(:name, :schedule)
+
+  def set_retailer
+      @retailer = Retailer.find(params[:id])
   end
+
+  def retailer_params
+      params.require(:retailer).permit(:name, :schedule)
+  end
+
 end
