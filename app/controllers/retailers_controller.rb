aabae8b15bb5f8ebc5ff1aeb7fc69ef84fa0372f
Aschonely <aschonely@gmail.com>
2020-03-15 20:22:51 -0400

    Fixed spacing issues in the methods

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -4,51 +4,50 @@ class RetailersController < ApplicationController
   before_action :set_retailer, only: [:show, :edit, :update, :destroy]
 
   def index
-     @retailers = Retailer.all
+    @retailers = Retailer.all
   end
 
   def show
-     @retailer = Retailer.find(params[:id])
+   @retailer = Retailer.find(params[:id])
   end
 
   def new
-      @retailer = Retailer.new
-      @retailer.finished_products.build
+    @retailer = Retailer.new
+    @retailer.finished_products.build
   end
 
   def edit
   end
 
   def create
-      @retailer = Retailer.new(retailer_params)
-      if @retailer.save
-          redirect_to retailer_path(@retailer)
-      else
-          render :new
-      end
+    @retailer = Retailer.new(retailer_params)
+    if @retailer.save
+      redirect_to retailer_path(@retailer)
+    else
+      render :new
+    end
   end
 
   def update
-      if @retailer.update(retailer_params)
-          redirect_to retailer_path(@retailer)
-      else 
-          render :edit
-      end
+    if @retailer.update(retailer_params)
+      redirect_to retailer_path(@retailer)
+    else 
+      render :edit
+    end
   end
 
   def destroy
-      @retailer.destroy
-      redirect_to retailers_path
+    @retailer.destroy
+    redirect_to retailers_path
   end
 
   private
 
   def set_retailer
-      @retailer = Retailer.find(params[:id])
+    @retailer = Retailer.find(params[:id])
   end
 
   def retailer_params
-      params.require(:retailer).permit(:name, :schedule)
+    params.require(:retailer).permit(:name, :schedule)
   end
-
 end
